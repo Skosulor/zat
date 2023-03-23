2db8f5519d0426b65e56d1707fcf7b9581ee6f8d
Andreas Öhman <andreas.ohman@>
2023-03-23 21:38:08 +0100

    Parse options

--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,78 @@
 const std = @import("std");
 
+const Flags = struct {
+    help: bool = false,
+    version: bool = false,
+    non_blank: bool = false,
+    non_print: bool = false,
+    number: bool = false,
+    squeeze: bool = false,
+};
+
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    var allocator = std.heap.page_allocator;
+    var args = try std.process.argsAlloc(allocator);
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    for (args[1..args.len]) |arg| {
+        std.debug.print("{s}\n", .{arg});
+    }
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    var flags = Flags{};
 
-    try bw.flush(); // don't forget to flush!
+
+    for (args[1..args.len]) |arg| {
+        if(compare(arg, "-h") or compare(arg, "--help")){
+            flags.help = true;
+        }
+        else if(compare(arg, "-v") or compare(arg, "--version")){
+            flags.version = true;
+        }
+        else if(compare(arg, "-b") or compare(arg, "--number-nonblank")){
+            flags.non_blank = true;
+        }
+        else if(compare(arg, "-v") or compare(arg, "--show-nonprint")){
+            flags.non_print = true;
+        }
+        else if(compare(arg, "-n") or compare(arg, "--number")){
+            flags.number = true;
+        }
+        else if(compare(arg, "-s") or compare(arg, "--squeeze-blank")){
+            flags.squeeze = true;
+        }
+    }
+
+    if(flags.help)
+        printHelp();
+
+
 }
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+fn compare(str1: []const u8, str2: []const u8) bool {
+    if(str1.len != str2.len)
+        return false;
+
+    for(str1) |char1, i| {
+        if(char1 != str2[i])
+            return false;
+    }
+
+    return true;
+}
+
+
+
+fn printHelp() void
+{
+        std.debug.print("Usage: zat [OPTION]... [FILE]...\n", .{});
+        std.debug.print("Concatenate FILE(s) to standard output.\n", .{});
+        std.debug.print("  -b, --number-nonblank    number nonempty output lines, overrides -n\n", .{});
+        std.debug.print("  -n, --number             number all output lines\n", .{});
+        std.debug.print("  -s, --squeeze-blank      suppress repeated empty output lines\n", .{});
+        std.debug.print("  -v, --show-nonprinting   use ^ and M- notation, except for LFD and TAB\n", .{});
+        std.debug.print("  -h  --help     display this help and exit\n", .{});
+        std.debug.print("  -v  --version  output version information and exit\n", .{});
+        std.debug.print("Examples:\n", .{});
+        std.debug.print("  zat f - g  Output f's contents, then standard input, then g's contents.\n", .{});
+        std.debug.print("  zat        Copy standard input to standard output.\n", .{});
+
 }
